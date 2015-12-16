72c0cff41cf70e730f4b049112388ec7c92bd36b
Adrienne Debigare <adebigare@gmail.com>
2015-12-15 22:37:11 -0500

    deleted unnecessary files

--- app/src/js/controllers/UnzombifyController.js
+++
@@ -1,5 +0,0 @@
-angular.module('translapi')
-.controller('UnzombifyController', ['$routeParams', '$location', function($routeParams, $location){
-	var uc = this; 
-
-}]);
--- app/src/js/services/translator.js
+++
@@ -1,3 +0,0 @@
-angular.module('translapi').factory('Translator', ['$resource', function($resource){
-  return $resource('http://localhost:7000/zombify');
-}]);
